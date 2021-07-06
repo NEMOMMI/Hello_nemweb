0ee5d5fc8bc2d35f7c4e920229b553d3e5bf715c
nem <lune213@gmail.com>
2021-07-06 19:27:45 +0900

    JUnit test pass(HelloControllerTest)

--- src/test/java/com/nemWeb/web/HelloControllerTest.java
+++ src/test/java/com/nemWeb/web/HelloControllerTest.java
@@ -6,15 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+//import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
 
+@WebMvcTest
 public class HelloControllerTest {
 
     @Autowired
@@ -29,4 +31,18 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @Test
+    public void helloDto_return() throws Exception { //helloDto가_리턴된다
+        String name = "helle";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                    .param("name", name)
+                    .param("amount", String.valueOf(amount))
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
 }
