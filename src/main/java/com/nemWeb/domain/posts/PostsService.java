7c6cadac8ffbc56844ed2926587a76beec277cd0
nem <lune213@gmail.com>
2021-07-08 18:57:07 +0900

    add PostsApiController, test pass

---
+++ src/main/java/com/nemWeb/domain/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.nemWeb.domain.posts;
+
+import com.nemWeb.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
