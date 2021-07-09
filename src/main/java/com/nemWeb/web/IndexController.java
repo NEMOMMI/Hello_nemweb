5780baf34ba05cc38c8bf147c02a1dbdfcfd9196
nem <lune213@gmail.com>
2021-07-09 18:15:13 +0900

    divide layout, add post page

--- src/main/java/com/nemWeb/web/IndexController.java
+++ src/main/java/com/nemWeb/web/IndexController.java
@@ -1,8 +1,10 @@
 package com.nemWeb.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 
 public class IndexController {
@@ -10,6 +12,11 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 
 }
