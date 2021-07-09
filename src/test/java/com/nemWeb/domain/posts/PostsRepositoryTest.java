edb759b11cab3626a26f3894863ca72d7333f5a2
nem <lune213@gmail.com>
2021-07-09 17:09:54 +0900

    add BaseTimeEntity, test pass

--- src/test/java/com/nemWeb/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/nemWeb/domain/posts/PostsRepositoryTest.java
@@ -46,4 +46,30 @@ public class PostsRepositoryTest {
         assertThat(posts.getContent()).isEqualTo(content);
 
     }
+
+    //123
+    @Test
+    public void BaseTimeEntity_등록() {
+
+        //given
+        LocalDateTime now = LocalDateTime.of(2021,7,9,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build()
+        );
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>> createDate" + posts.getCreatedDate() +", modifiedDate" +posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
+    }
+
 }
