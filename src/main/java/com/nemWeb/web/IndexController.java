718a44c8ed228ed8fb462863e16f40d0ac47b489
nem <lune213@gmail.com>
2021-07-12 14:09:31 +0900

    add google login btn

--- src/main/java/com/nemWeb/web/IndexController.java
+++ src/main/java/com/nemWeb/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.nemWeb.web;
 
+import com.nemWeb.config.auth.dto.SessionUser;
 import com.nemWeb.domain.posts.PostsService;
 import com.nemWeb.web.dto.PostsResponseDto;
 import lombok.Getter;
@@ -9,21 +10,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        //190
+        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
-//    public String index() {
-//        return "index";
-//    }
 
     @GetMapping("/posts/save")
     public String postsSave() {
