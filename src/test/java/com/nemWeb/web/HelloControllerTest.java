f0a4d2a0e007ca54ad9518c1f255f9d0f07df506
nem <lune213@gmail.com>
2021-07-15 16:53:54 +0900

    Tests passed:7, fail:1(hello_return)

--- src/test/java/com/nemWeb/web/HelloControllerTest.java
+++ src/test/java/com/nemWeb/web/HelloControllerTest.java
@@ -1,12 +1,15 @@
 package com.nemWeb.web;
 
+import com.nemWeb.config.auth.SecurityConfig;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -14,14 +17,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+import org.springframework.security.test.context.support.WithMockUser;
 
-@WebMvcTest
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello_return() throws Exception { //hello가_리턴된다
         String hello = "hello";
@@ -31,6 +40,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto_return() throws Exception { //helloDto가_리턴된다
         String name = "helle";
@@ -38,11 +48,12 @@ public class HelloControllerTest {
 
         mvc.perform(
                 get("/hello/dto")
-                    .param("name", name)
-                    .param("amount", String.valueOf(amount))
-        )
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
+
     }
+
 }
