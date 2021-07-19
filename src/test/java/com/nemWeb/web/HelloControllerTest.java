09192124ef8329f5036885c1422d8a2ca6f210a1
nem <lune213@gmail.com>
2021-07-19 14:04:20 +0900

    Tests passed:8

--- src/test/java/com/nemWeb/web/HelloControllerTest.java
+++ src/test/java/com/nemWeb/web/HelloControllerTest.java
@@ -6,8 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,8 +18,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.springframework.security.test.context.support.WithMockUser;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class,
@@ -39,6 +39,7 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
+
 
     @WithMockUser(roles = "USER")
     @Test
